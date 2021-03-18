59305d63380e712768007fd3587ef4e394205b31
Sunilnani <42605686+Sunilnani@users.noreply.github.com>
2021-03-18 10:46:02 +0530

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'camera.dart';
 
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
       title: 'Flutter Demo',
       theme: ThemeData(
 
@@ -30,6 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String tabColor = "1";
   int _selectedIndex = 0;
   List<String> names= ["Sunil","sandy","sunny","prasad","Sunil","sandy","sunny","prasad","Sunil","sandy","sunny","prasad","Sunil","sandy","sunny","prasad",];
   List<String> images=["img/img1.jpg","img/img2.jpg","img/img3.jpg","img/img4.jpg","img/img5.jpg","img/img6.jpg","img/img7.jpg","img/img8.jpg","img/img1.jpg","img/img2.jpg","img/img3.jpg","img/img4.jpg","img/img5.jpg","img/img6.jpg","img/img7.jpg","img/img8.jpg",];
@@ -42,119 +44,163 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 15,vertical: 15),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Center(
+
+            child: Text(
+                "Edit",
+                style: TextStyle(color: Colors.deepPurple,fontWeight: FontWeight.w600)),),),
+
+        centerTitle: true,
+        title: Text(
+            "Calls",
+            style:TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.w700) ),
+        actions: [
+          Icon(
+            Icons.more_vert,
+            color: Colors.purple,
+            size: 25,
+          ),
+          SizedBox(width: 20,)
+        ],
+      ),
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 15,vertical: 15),
+              child: SingleChildScrollView(
+                child: Column(
                   children: [
-                    Text("Edit",style: TextStyle(color: Colors.deepPurple,fontWeight: FontWeight.w600),),
-                    Text("Calls",style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.w700),),
-                    Icon(Icons.more_vert,color: Colors.deepPurple,size: 20,)
+                    // Row(
+                    //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //   children: [
+                    //     Text("Edit",style: TextStyle(color: Colors.deepPurple,fontWeight: FontWeight.w600),),
+                    //     Text("Calls",style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.w700),),
+                    //     Icon(Icons.more_vert,color: Colors.deepPurple,size: 20,)
+                    //   ],
+                    // ),
+                    Container(
+                      height: 50,
+                      width: 350,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            child: InkWell(
+                              onTap: (){
+                                setState(() {
+                                  tabColor = "1";
+                                });
+                              },
+                              child: Container(
+
+                                height: 50,
+                                width: 175,
+                                decoration: BoxDecoration(
+                                    color: tabColor == "1" ?  Colors.white : null,
+                                    borderRadius: BorderRadius.circular(30)
+                                ),
+                                child: Center(child: Text("All" , style: TextStyle(color: tabColor == "1" ?  Colors.purple : Colors.grey ,fontSize: 12),)),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            right:0,
+                            child: InkWell(
+                              onTap: (){
+                                setState(() {
+                                  tabColor = "2";
+                                });
+                              },
+                              child: Container(
+                                height: 50,
+                                width: 185,
+
+                                decoration: BoxDecoration(
+                                    color : tabColor == "2" ?  Colors.white : null,
+                                    borderRadius: BorderRadius.circular(30)
+                                ),
+                                child: Center(child: Text("Missed" ,style: TextStyle(color:  tabColor == "2" ?  Colors.purple : Colors.grey  , fontSize: 12),)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(30),
+                        color: Color(0xfff8f8fa),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey[300],
+                            offset: const Offset(
+                              1.0,
+                              1.0,
+                            ),
+                            blurRadius: 1,
+
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Container(
+                      child: ListView.builder(
+                        itemCount: names.length,
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return Calling(
+                            name: names[index],
+                            image: images[index],
+                          );
+                        },
+                      ),
+                    ),
                   ],
                 ),
-                SizedBox(height: 25,),
-                Container(
-                  height: 50,
-                  width: 350,
-                  child: Stack(
-                    children: [
-
-                      Positioned(
-                        left: 0,
-                        child: Container(
-
-                          height: 50,
-                          width: 175,
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(30)
-                          ),
-                          child: Center(child: Text("All" , style: TextStyle(color: Colors.purple),)),
-                        ),
-                      ),
-                      Positioned(
-                        right:0,
-                        child: Container(
-                          height: 50,
-                          width: 185,
-
-                          decoration: BoxDecoration(
-
-                              borderRadius: BorderRadius.circular(30)
-                          ),
-                          child: Center(child: Text("hai" ,style: TextStyle(color: Colors.grey),)),
-                        ),
-                      ),
-                    ],
-                  ),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.grey[300],
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey[300],
-                        offset: const Offset(
-                          2.0,
-                          2.0,
-                        ),
-                        blurRadius: 5.0,
-                        spreadRadius: 1.0,
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 15,),
-                Container(
-                  child: ListView.builder(
-                    itemCount: names.length,
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return Calling(
-                        name: names[index],
-                        image: images[index],
-                      );
-                    },
-                  ),
-                )
-              ],
+              ),
             ),
           ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.call_outlined,color: Colors.purple,),
-              label: ""
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.message_outlined,color: Colors.grey,),
-              label: ""
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.dialpad_outlined,size: 25,color: Colors.purple,),
-              label: ""
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.perm_identity_rounded,color: Colors.grey,),
-              label: ""
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.more_horiz,color: Colors.grey,),
-            label: "",
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                height: 60,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+
+                    borderRadius: BorderRadius.only(topRight: Radius.circular(25) , topLeft: Radius.circular(25)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(-1, -3),
+                          blurRadius: 6)
+                    ]),
+
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(Icons.phone , color: Colors.purple, size: 20,),
+                    Icon(Icons.message , color: Colors.black54,size: 20,),
+                    CircleAvatar(
+                      radius: 23,
+                      child: Icon(Icons.dialpad_outlined,size: 25,color: Colors.purple,),
+                    ),
+                    Icon(Icons.person ,color: Colors.black54, size: 20,),
+                    Icon(Icons.more_horiz ,color: Colors.black54,size: 20,),
+                  ],
+                )
+            ),
           ),
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.purple,
-        onTap: _onItemTapped,
-      ),// This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
